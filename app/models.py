b939b12bc678faa356a5ee04bdbb3138cbe30e92
ErikBoesen <me@erikboesen.com>
2022-11-25 14:23:22 -0500

    Remove invited_me property

--- app/models.py
+++ app/models.py
@@ -505,7 +505,6 @@ class Event(db.Model):
             'end_time': self.end_time.isoformat() if self.end_time else None,
             'happening_now': self.happening_now(),
             'mine': me.admin or self.is_hosted_by(me),
-            'invited_me': self.is_invited(me),
             'people': self.people(),
             'review': review.json() if review else None,
             'rating': self.rating(),
